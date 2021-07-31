1d3a43f05164615c07d0ebeeb877db37c0aa3aa3
gmldnjs26 <imleesky265@gmail.com>
2021-07-31 13:59:26 +0900

    form and CSRF setting

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,6 +1,10 @@
 class CalculatorController < ApplicationController
   def add
-    # @는 view file과 정보를 공유할 수 있는 변수
+    ## @는 view file과 정보를 공유할 수 있는 변수
     @message = "Hi I'am Information" 
+  end
+
+  def result
+    @result = params[:first].to_i + params[:second].to_i
   end
 end
